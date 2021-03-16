5a7c63458ee494bed4358775a039fa1bc77ae73c
MattSnyk <74914681+MattSnyk@users.noreply.github.com>
2021-03-16 23:56:33 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -16,6 +16,13 @@ resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
+ root_block_device {
+    volume_size           = "20"
+    volume_type           = "gp2"
+    encrypted             = true
+    delete_on_termination = true
+  
+
   tags = {
     Name = "ExampleInstance"
   }
