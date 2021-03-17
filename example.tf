97ade2126c95332b485585ccb95fffc222002d03
MattSnyk <74914681+MattSnyk@users.noreply.github.com>
2021-03-17 09:31:40 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -17,13 +17,31 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
  root_block_device {
-  #  volume_size           = "20"
-  #  volume_type           = "gp2"
+#    volume_size           = "20"
+    volume_type           = "gp2"
     encrypted             = false
     delete_on_termination = true
-   }
+  }
 
   tags = {
     Name = "ExampleInstance"
   }
+}
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "pleasebedamnuniquealready" {
+  bucket = "pleasebedamnuniquealready"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
 }
